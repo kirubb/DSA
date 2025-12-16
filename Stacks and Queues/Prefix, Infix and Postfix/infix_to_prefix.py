42344df08bfe74d7a7a1bf0700ed12563156c542
Kirit Baruah <125810987+kirubb@users.noreply.github.com>
2025-12-16 19:22:27 +0530

    infix to prefix

--- Stacks and Queues/Prefix, Infix and Postfix/infix_to_prefix.py
+++ Stacks and Queues/Prefix, Infix and Postfix/infix_to_prefix.py
@@ -1,8 +1,11 @@
-def infix_to_postfix(expr):
+def infix_to_prefix(expr):
+    # reverse expression and swap brackets
+    expr = expr[::-1]
+    expr = expr.replace('(', '#').replace(')', '(').replace('#', ')')
+
     stack = []
     postfix = []
 
-    # precedence
     prec = {
         '+': 1,
         '-': 1,
@@ -24,35 +27,28 @@ def infix_to_postfix(expr):
         elif ch == ')':
             while stack and stack[-1] != '(':
                 postfix.append(stack.pop())
-            stack.pop()   # remove '('
+            stack.pop()
 
         # operator
         else:
             while stack and stack[-1] != '(':
                 top = stack[-1]
 
-                # higher precedence
-                if prec[top] > prec[ch]:
+                # NOTE: here ^ is treated as LEFT associative
+                if prec[top] > prec[ch] or prec[top] == prec[ch]:
                     postfix.append(stack.pop())
-
-                # same precedence & left associative
-                elif prec[top] == prec[ch] and ch != '^':
-                    postfix.append(stack.pop())
-
                 else:
                     break
 
             stack.append(ch)
 
-    # pop remaining operators
     while stack:
         postfix.append(stack.pop())
 
-    return ''.join(postfix)
+    # reverse postfix to get prefix
+    return ''.join(postfix[::-1])
 
 
 # Example
-print(infix_to_postfix("a^b^c"))
-print(infix_to_postfix("(a+b)*c"))
-print(infix_to_postfix("a+b*c"))
-print(infix_to_postfix("a+b-bb*(c^d^e-f)"))
+print(infix_to_prefix("a+b-*(c^d^e-f)"))
+print(infix_to_prefix("(a+b)*c-d+e"))
